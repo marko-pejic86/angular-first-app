ce1642402b1d656d4733188dc4af100225dfe828
Marko Pejic <marko@felicons.com>
2019-04-03 23:29:32 +0200

    Added more functionality to the application

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe (
+      'Test Recipe',
+      'This is just a test recipe',
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/image/recipes/sl/13/07/firecracker-grilled-salmon-sl-x.jpg?itok=DXNiVdiS'
+    )
+  ];
 
   constructor() { }
 
